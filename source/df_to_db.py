6b3083edf686e7a0e22517bf08e5c2afa13f82eb
GayathriSrividya <gayathrirajavarapu@sanketika.in>
2022-09-01 12:39:15 +0530

    python code to convert csv to postgresql table

--- source/df_to_db.py
+++ source/df_to_db.py
@@ -4,34 +4,16 @@ import os
 import psycopg2 as ps
 import pandas as pd
 from config import config
+from crudclass import CrudOnDatabase
 from datetime import datetime
+
 
 # reading csv file 
 
 print("reading file ratings.csv...\n")
 print("converting csv file into pandas dataframe...\n")
-df=pd.read_csv("ratings.csv", encoding='latin')
+df=pd.read_csv("../data/ratings.csv", encoding='latin')
 df['Date Rated'] = pd.to_datetime(df['Date Rated'])
-
-# declaring function to insert column values to table
-
-def insert_into_table(cursor, Const, Your_Rating, Date_Rated, Title, URL, Title_Type, IMDb_Rating, Runtime_mins, 
-   Year, Genres, Num_Votes, Release_Date, Directors):
-   columns=("""INSERT INTO movies (Const, Your_Rating, Date_Rated, Title, URL, Title_type, IMDb_Rating, Runtime_mins, 
-   Year, Genres, Num_Votes, Release_Date, Directors) 
-   VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);""")
-   rows=(Const, Your_Rating, Date_Rated, Title, URL, Title_Type, IMDb_Rating, Runtime_mins, Year, 
-   Genres, Num_Votes, Release_Date, Directors)
-   cursor.execute(columns, rows)
- 
-# declaring function to append values  
-
-def append_to_db(cursor, df):
-   for index, row in df.iterrows():
-       insert_into_table(cursor, row['Const'], row['Your Rating'], row['Date Rated'], row['Title'], row['URL'], 
-       row['Title Type'], row['IMDb Rating'], row['Runtime (mins)'], row['Year'], row['Genres'], row['Num Votes'], 
-       row['Release Date'], row['Directors'])
-   print("Values are inserted into the database Successfully \n")
 
 # reading the configuration parameters by calling config function
 
@@ -46,7 +28,7 @@ try:
     
     # declaring a cursor
 
-    cursor = conn.cursor();
+    cursor = conn.cursor()
     print("Connected to database...\n")
 
    # deleting the table if already existed in the database 
@@ -56,11 +38,11 @@ try:
    # writing a sql query to create table movies 
 
     create_table = '''CREATE TABLE movies(
-    Const VARCHAR(50) NOT NULL,
+    Const VARCHAR(50) NOT NULL PRIMARY KEY,
     Your_Rating FLOAT NOT NULL,
     Date_Rated DATE NOT NULL,
     Title VARCHAR(255) NOT NULL,
-    URL VARCHAR(255) NOT NULL,
+    URL VARCHAR(50) NOT NULL,
     Title_Type VARCHAR(50) NOT NULL,
     IMDb_Rating VARCHAR(50) NOT NULL,
     Runtime_mins FLOAT NOT NULL,
@@ -74,17 +56,24 @@ try:
    # executing the query
    # after successful execution a table named movies will be created in the database
 
-    cursor.execute(create_table);
+    cursor.execute(create_table)
+    print("inserting values into database...\n")
+    for i, row in df.iterrows():
+        # creating class instance to handle CRUD functions on database
+         
+        this_movie = CrudOnDatabase(row['Const'], row['Your Rating'], row['Date Rated'], row['Title'], row['URL'], row['Title Type'], row['IMDb Rating'], row['Runtime (mins)'], row['Year'], row['Genres'], row['Num Votes'], row['Release Date'], row['Directors'])
+        this_movie.insert()
+    print("Values are inserted into the database Successfully \n")
 
-  # calling append_to_db function to insert values in dataframe to database
+    # commiting the changes
 
-    append_to_db(cursor, df)
     conn.commit()
-    cursor.execute("SELECT * FROM movies;")
 
- # closing the cursor
+    # closing the cursor
 
     cursor.close()
+
+# exception block print the error if any
 
 except (Exception, ps.DatabaseError) as error:
         print(error)
@@ -94,5 +83,12 @@ except (Exception, ps.DatabaseError) as error:
 finally:
     if conn is not None:
         conn.close()
-        print("Database Connection is closed..")
+        print("Database Connection is closed...\n")
 
+
+# below statements are examples how to call crud functions using class instance
+
+# this_movie.update('tt1001526', 8.23)
+# this_movie.update('tt1001526', 18.23)
+# this_movie.read('tt1001526')
+# this_movie.delete('tt1001526')
