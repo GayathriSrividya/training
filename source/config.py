4930c0f180b0711f7787575b0b9455c4f220a406
GayathriSrividya <gayathrirajavarapu@sanketika.in>
2022-09-01 12:38:09 +0530

    configurations for postgreSQL server

--- source/config.py
+++ source/config.py
@@ -1,7 +1,7 @@
 from configparser import ConfigParser
 
 
-def config(name='database.ini', section='postgresql'):
+def config(name='../database.ini', section='postgresql'):
     # create a parser
     parser = ConfigParser()
     # read config file
