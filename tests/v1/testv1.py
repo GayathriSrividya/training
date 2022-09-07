de0c8ff92ee240044215dd913da49cab547fc5cb
GayathriSrividya <gayathrirajavarapu@sanketika.in>
2022-09-07 14:22:02 +0530

    unit test cases

---
+++ tests/v1/testv1.py
@@ -0,0 +1,25 @@
+import unittest
+import json
+import sys
+from unittest.util import three_way_cmp
+sys.path.append('/home/stpl/gayathri/training/src')
+from v1.ratings_class import Ratings
+
+class TestRating(unittest.TestCase):
+    file_json= open('/home/stpl/gayathri/training/tests/v1/v1params.json')
+    data = json.load(file_json)
+    instance = Ratings("tt0100802", 3, 4/19/2015, "Total Recall", "https://www.imdb.com/title/tt0100802/", "movie", 7.5, 113.0, 1990, "https://www.imdb.com/title/tt0100802/", 278834, "5/31/1990", "Paul Verhoeven")
+
+    def test_set_rating(self):
+        for query, text in self.data['set'].items():
+            print(query+"\n")
+            self.assertEqual(self.instance.set_rating(text[0]), text[1])
+
+    def test_get_rating(self):
+
+        for query, text in self.data['get'].items():
+            print(query+'\n')
+            self.assertEqual(self.instance.get_rating(text[0]), text[1])
+         
+         
+unittest.main()
---
+++ tests/v2/testv2.py
@@ -0,0 +1,33 @@
+import unittest
+import json
+import sys
+sys.path.append("/home/stpl/gayathri/training/src")
+from v2.records_class import Records
+
+class Testrecords(unittest.TestCase):
+
+    instance = Records()  
+    file_json= open('/home/stpl/gayathri/training/tests/v2/v2params.json')
+    data = json.load(file_json)
+
+    def test_insert(self):
+        for query, text in self.data['insert'].items():
+            print(query+"\n")
+            self.assertEqual(self.instance.insert(text[0]), text[1])
+
+    def test_read(self):
+        for query, text in self.data['read'].items():
+            print(query+"\n")
+            self.assertEqual(self.instance.read(text[0], text[1]), text[2])
+   
+    def test_update(self):
+        for query, text in self.data['update'].items():
+            print(query+"\n")
+            self.assertEqual(self.instance.update(text[0], text[1]), text[2])
+
+    def test_delete(self):
+        for query, text in self.data['delete'].items():
+            print(query+"\n")
+            self.assertEqual(self.instance.delete(text[0]), text[1])
+            
+unittest.main()
