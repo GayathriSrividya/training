4c7cb690979f4bf90aaee4ee8d49a56a67cc61ba
GayathriSrividya <gayathrirajavarapu@sanketika.in>
2022-09-07 14:29:05 +0530

    python code to convert pandas dataframe to postgreSQL database

--- src/v2/dataframe.py
+++ src/v2/dataframe.py
@@ -1,17 +1,19 @@
 # importing required modules
 import json
+
 import pandas as pd
+
 from records_class import Records
 
 # reading csv file 
 
 print("reading file ratings.csv...\n")
 print("converting csv file into pandas dataframe...\n")
-df=pd.read_csv("../../data/ratings.csv", encoding='latin')
+df=pd.read_csv("/home/stpl/gayathri/training/data/ratings.csv", encoding='latin')
 df['Date Rated'] = pd.to_datetime(df['Date Rated'])
 print("csv file is successfully converted into dataframe...\n")
-
-# creating instance for ManageRecords class for CRUD operations
+ 
+# creating instance for Records class
  
 this_movie=Records()
 
@@ -21,10 +23,9 @@ for index, row in df.iterrows():
     this_movie.insert(row)
 
 print("Values are inserted into the database Successfully. \n")
-this_movie.delete('tt1001526')
 
-file_json= open('../../config/test_query.json')
+file_json= open('/home/stpl/gayathri/training/config/test_query.json')
 data = json.load(file_json)
-for sqltext in data.values():
+for query, sqltext in data.items():
+    print(query+"\n")
     this_movie.read(sqltext, 'tt1001526')
-
