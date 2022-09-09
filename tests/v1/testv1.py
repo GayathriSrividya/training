2f33c5e7e3fe941ee2798a55b98ff11c52310a74
GayathriSrividya <gayathrirajavarapu@sanketika.in>
2022-09-09 10:01:11 +0530

    uniitest cases

--- tests/v1/testv1.py
+++ tests/v1/testv1.py
@@ -1,12 +1,11 @@
 import unittest
 import json
 import sys
-from unittest.util import three_way_cmp
-sys.path.append('/home/stpl/gayathri/training/src')
-from v1.ratings_class import Ratings
+sys.path.append('../../src')
+from v1.ratings import Ratings
 
 class TestRating(unittest.TestCase):
-    file_json= open('/home/stpl/gayathri/training/config/v1params.json')
+    file_json= open('../../tests/config/v1params.json')
     data = json.load(file_json)
     instance = Ratings("tt0100802", 3, 4/19/2015, "Total Recall", "https://www.imdb.com/title/tt0100802/", "movie", 7.5, 113.0, 1990, "https://www.imdb.com/title/tt0100802/", 278834, "5/31/1990", "Paul Verhoeven")
 
@@ -19,7 +18,7 @@ class TestRating(unittest.TestCase):
 
         for query, text in self.data['get'].items():
             print(query+'\n')
-            self.assertEqual(self.instance.get_rating(text[0]), text[1])
+            self.assertEqual(self.instance.get_rating(), text[0])
          
 if __name__ == '__main__':  
     unittest.main()
