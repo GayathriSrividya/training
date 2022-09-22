3a8816628986da4f03d87c05092eeb09db5a04da
GayathriSrividya <gayathrirajavarapu@sanketika.in>
2022-09-22 10:34:28 +0530

    querying on data with pyspark

--- pyspark_sql/csv2pyspark.py
+++ pyspark_sql/csv2pyspark.py
@@ -3,6 +3,7 @@ from pyspark.sql import SparkSession
 from pyspark.sql.functions import *
 
 spark=SparkSession.builder.appName("pyspark_sql").getOrCreate()
+spark.sql("set spark.sql.legacy.timeParserPolicy=LEGACY")
 ratings=spark.read.csv('../data/ratings.csv', header=True, inferSchema=True)
 pyspark_queries=open('../config/pyspark_queries.json')
 query_data=json.load(pyspark_queries)
