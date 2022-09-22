5bcc493b06c82ddcb7f5812c437c46f3893e549c
GayathriSrividya <gayathrirajavarapu@sanketika.in>
2022-09-22 10:16:11 +0530

    querying on data with pyspark

---
+++ pyspark_sql/csv2pyspark.py
@@ -0,0 +1,14 @@
+import json
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import *
+
+spark=SparkSession.builder.appName("pyspark_sql").getOrCreate()
+ratings=spark.read.csv('../data/ratings.csv', header=True, inferSchema=True)
+pyspark_queries=open('../config/pyspark_queries.json')
+query_data=json.load(pyspark_queries)
+
+# executing pyspark queries one by one
+
+for query, pdtxt in query_data.items():
+    print(query+':\n')
+    exec(pdtxt)
